9d28d7755af1c4dbd434f855d48a91e13c03172f
Uki Mahfuda <mahfuda948@gmail.com>
2023-12-05 11:13:10 +0700

    update login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,6 +18,9 @@
     <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/normalize.css') }}" />
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}"> --}}
 
 </head>
 
@@ -91,8 +94,26 @@
     navbarToggler.addEventListener('click', function() {
         navbarToggler.classList.toggle("active");
     });
+
+    function togglePassword() {
+        var x = document.getElementById("password-content-3-6");
+        if (x.type === "password") {
+            x.type = "text";
+            document
+                .getElementById("icon-toggle")
+                .setAttribute("fill", "#2ec49c");
+        } else {
+            x.type = "password";
+            document
+                .getElementById("icon-toggle")
+                .setAttribute("fill", "#CACBCE");
+        }
+    }
 </script>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+</script>
 
 
 </body>
